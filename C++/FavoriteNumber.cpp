33915bdfca4dc911361f45a48f04f82a58724ca3
Sukuna963 <86301498+Sukuna963@users.noreply.github.com>
2023-11-26 11:06:04 -0300

    *

---
+++ C++/FavoriteNumber.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int number;
+
+	cout << "Enter number: " << endl;
+	cin >> number;
+	cout << "Your " << number << " is my favorite number too." << endl;
+
+	return 0;
+}
