81c85da20fb100e3108a9c6078effd9e1b7605e1
Sukuna963 <86301498+Sukuna963@users.noreply.github.com>
2023-11-28 21:54:37 -0300

    code challenges

--- CodeWars/evenOrOdd.java
+++ CodeWars/evenOrOdd.java
@@ -1,3 +1,9 @@
+//////////////////////////////////////////////////
+// Author: Leonardo Marques
+// Codewars: 8 kyu
+//java 17
+//////////////////////////////////////////////////
+
 public class Kata {
     public static String evenOrOdd(int number) {
         return ""; // Place code here;
