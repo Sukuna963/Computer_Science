57b499caa972fae534047b88953c4f99fabe56df
Sukuna963 <marquesleonardo284@gmail.com>
2023-12-13 20:22:08 -0300

    coding challenges

---
+++ LeetCode/twoSum.cpp
@@ -0,0 +1,36 @@
+/***************************************************************
+* Author: Leonardo Marques
+* leetcode: Easy
+***************************************************************/
+
+/* RunTime = 3ms
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        for(int i=0;; ++i){
+            int x= nums[i];
+            int y= target - x;
+            if(m.count(y)) {
+                return {m[y], i};
+            }
+            m[x] =i;
+        }
+    }
+};
+*/
+
+// RunTime = 307 ms
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = i+1; j < nums.size(); j++) {
+                if(nums[i] + nums[j] == target) {
+                    return {i,j};
+                }
+            }
+        }
+        return {};
+    }
+};
